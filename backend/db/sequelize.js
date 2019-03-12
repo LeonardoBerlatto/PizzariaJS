3d804aa2f97fa9b043df0e2d1df01c3b87d56abc
Doctor-28 <leoberlatto@gmail.com>
2019-03-12 20:34:09 -0300

    Fixing sequelize connection

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,11 +1,14 @@
 const Sequelize = require('sequelize');
+const logger = require('../logger');
+
 const sequelize = new Sequelize({
 	database: 'pizzaria',
 	username: 'root',
 	password: null,
+	logging: false,
 	dialect: 'mysql',
 	host: 'localhost',
-	port: 3600,
+	port: 3306,
 
 	pool: {
 		max: 5,
@@ -13,6 +16,10 @@ const sequelize = new Sequelize({
 		acquire: 30000,
 		idle: 10000
 	},
+});
+
+sequelize.sync().then(()=>{
+	logger.info('Connection established successfully');
 });
 
 module.exports = sequelize;
