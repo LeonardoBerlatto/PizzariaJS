226f28cffc05357dd7d0e193c4b36b421208e74f
Doctor-28 <leoberlatto@gmail.com>
2019-03-15 18:30:35 -0300

    Added routes to root

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const UserRouter = require('./routes/user');
 const FranchiseRouter = require('./routes/franchise');
+const PurchaseRouter = require('./routes/purchase');
+const FlavorRouter = require('./routes/flavor');
 
 const app = express();
 app.use(express.json());
@@ -8,5 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', UserRouter);
 app.use('/api/franchises', FranchiseRouter);
+app.use('/api/purchases', PurchaseRouter);
+app.use('/api/flavors', FlavorRouter);
 
 module.exports = app;
