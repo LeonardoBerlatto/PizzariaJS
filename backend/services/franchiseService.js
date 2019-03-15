1b4adf3e1f83a06930f1e1ffce0fa5e37e943a52
Doctor-28 <leoberlatto@gmail.com>
2019-03-14 22:12:43 -0300

    Creating frachise service

---
+++ backend/services/franchiseService.js
@@ -0,0 +1,76 @@
+const Franchise = require('../models/franchise');
+const NotFoundError = require('../exceptions/NotFoundError');
+const InvalidInput = require('../exceptions/InvalidInput');
+const FranchiseNameAlreadyExistsError = require('../exceptions/FranchiseNameAlreadyExistsError');
+const User = require('../models/user');
+
+async function getFranchiseById(id) {
+	const franchise = await Franchise.findByPk(id);
+	if (!franchise) {
+		throw new NotFoundError('Franchise');
+	}
+	return franchise;
+}
+
+async function createFranchise(data) {
+	const franchise = await Franchise.findOne({
+		where: {
+			name: data.name
+		}
+	});
+	if (franchise) {
+		throw new FranchiseNameAlreadyExistsError();
+	}
+	const user = User.findByPk(data.userId);
+	if (!user) {
+		throw new NotFoundError('User');
+	}
+
+	try {
+		const franchise = await Franchise.create(data);
+		return franchise;
+	} catch (error) {
+		throw new InvalidInput(error.message);
+	}
+}
+
+async function updateFranchise(id, dataToUpdate) {
+	const franchise = await Franchise.findByPk(id);
+	if (!franchise) {
+		throw new NotFoundError('Franchise');
+	}
+
+	try {
+		await Franchise.update(
+			dataToUpdate, {
+				returning: true,
+				where: {
+					id
+				}
+			});
+		const updatedUser = await Franchise.findByPk(id);
+		return updatedUser;
+	} catch (error) {
+		throw new InvalidInput(error.message);
+	}
+}
+
+async function deleteFranchise(id) {
+	const franchise = await Franchise.findByPk(id);
+	if (!franchise) {
+		throw new NotFoundError('Franchise');
+	}
+
+	await Franchise.destroy({
+		where: {
+			id
+		}
+	});
+
+	return franchise;
+}
+
+module.exports.getFranchiseById = getFranchiseById;
+module.exports.createFranchise = createFranchise;
+module.exports.updateFranchise = updateFranchise;
+module.exports.deleteFranchise = deleteFranchise;
