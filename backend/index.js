6d9787f2cc9cc3e61eca3671ac6a481aebb80604
Doctor-28 <leoberlatto@gmail.com>
2019-03-17 20:08:15 -0300

    Added endpoints to Ingredient

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const UserRouter = require('./routes/user');
 const FranchiseRouter = require('./routes/franchise');
 const PurchaseRouter = require('./routes/purchase');
 const FlavorRouter = require('./routes/flavor');
+const IngredientRouter = require('./routes/ingredient');
+
 
 const app = express();
 app.use(express.json());
@@ -12,5 +14,7 @@ app.use('/api/users', UserRouter);
 app.use('/api/franchises', FranchiseRouter);
 app.use('/api/purchases', PurchaseRouter);
 app.use('/api/flavors', FlavorRouter);
+app.use('/api/ingredients', IngredientRouter);
+
 
 module.exports = app;
---
+++ backend/routes/ingredient.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const IngredientService = require('../services/ingredientService');
+const {
+	CREATED,
+	OK
+} = require('../utils/statusCodes');
+
+
+router.get('/:id', async (req, res) => {
+	try {
+		const ingredient = await IngredientService.getIngredientById(req.params.id);
+		res.send(ingredient);
+	} catch (error) {
+		res.status(error.status);
+		res.send(error.message);
+	}
+});
+
+router.get('/flavor/:id', async (req, res) => {
+	try {
+		const ingredients = await IngredientService.getIngredientsFromFlavor(req.params.id);
+		res.send(ingredients);
+	} catch (error) {
+		res.status(error.status);
+		res.send(error.message);
+	}
+});
+
+router.post('/', async (req, res) => {
+	try {
+		const ingredient = await IngredientService.createIngredient(req.body);
+		res.status(CREATED);
+		res.send(ingredient);
+	} catch (error) {
+		res.status(error.status);
+		res.send(error.message);
+	}
+});
+
+router.patch('/:id', async (req, res) => {
+	try {
+		const ingredient = await IngredientService.updateIngredient(req.params.id, req.body);
+		res.status(OK);
+		res.send(ingredient);
+	} catch (error) {
+		res.status(error.status);
+		res.send(error.message);
+	}
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const ingredient = await IngredientService.deleteIngredient(req.params.id);
+		res.send(ingredient);
+	} catch (error) {
+		res.status(error.status);
+		res.send(error.message);
+	}
+});
+
+module.exports = router;
