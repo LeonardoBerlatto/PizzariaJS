58bb1dcc92580e58d9a22e1bfe780dd1a611c5e4
Doctor-28 <leoberlatto@gmail.com>
2019-03-19 16:56:12 -0300

    Adding auth service

---
+++ backend/services/authService.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const {
+	User
+} = require('../models/user');
+const UnauthorizedError = require('../exceptions/UnauthorizedError');
+const InvalidInputError = require('../exceptions/InvalidInputError');
+
+
+module.exports.login = async (userToLogin) => {
+	const {
+		error
+	} = validateUser(userToLogin);
+	if (error)
+		throw new InvalidInputError(error.details[0].message);
+
+	let user = await User.findOne({
+		email: userToLogin.email
+	});
+	if (!user)
+		throw new UnauthorizedError();
+
+	const validPassword = await bcrypt.compare(userToLogin.password, user.password);
+	if (!validPassword)
+		throw new UnauthorizedError();
+
+	return user;
+};
+
+function validateUser(req) {
+	const schema = {
+		email: Joi.string().min(6).max(50).email().required(),
+		password: Joi.string().min(8).max(32).required()
+	};
+	return Joi.validate(req, schema);
+}
