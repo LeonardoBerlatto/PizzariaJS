093288e4f3d41b972ea9b9f39164f8deb994a57e
Doctor-28 <leoberlatto@gmail.com>
2019-03-20 00:12:00 -0300

    Root structure and styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view />
+	</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+	#app {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+			Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		background-color: #fafafa;
+	}
+	#nav {
+		padding: 30px;
+		a {
+			font-weight: bold;
+			color: #2c3e50;
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
+
+	* {
+		box-sizing: border-box;
+	}
+
+	html,
+	body {
+		width: 100%;
+		margin: 0;
+		height: 100%;
+	}
 </style>
