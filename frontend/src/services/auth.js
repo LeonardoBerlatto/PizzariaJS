56c65e5cefad1c38d62fa45829952ea7be5afc9a
Doctor-28 <leoberlatto@gmail.com>
2019-03-20 17:39:04 -0300

    Adding auth service

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,9 @@
+import http from './http-client'
+
+export function login(userToLogin) {
+	return http.post('/auth', userToLogin);
+}
+
+export function checkLogin() {
+	return !!localStorage.getItem('token');
+}
---
+++ frontend/src/services/http-client.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const client = axios.create({
+	baseURL: 'http://127.0.0.1:9000/api',
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer '+ localStorage.getItem('token')
+	}
+});
+
+export default client;
