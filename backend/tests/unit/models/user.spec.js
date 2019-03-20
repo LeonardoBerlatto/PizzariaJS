28c9ad2c6a4b30041bb821050fcb138071e069c2
Doctor-28 <leoberlatto@gmail.com>
2019-03-19 21:49:49 -0300

    Updated user test

--- backend/tests/unit/models/user.spec.js
+++ backend/tests/unit/models/user.spec.js
@@ -1,6 +1,10 @@
 require('mysql2/node_modules/iconv-lite').encodingExists('foo');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 const {
-	validateUser
+	validateUser,
+	generateAuthToken
 } = require('../../../models/user');
 
 describe('validateUser function', () => {
@@ -23,5 +27,26 @@ describe('validateUser function', () => {
 		};
 		const res = validateUser(data);
 		expect(res.error.message).toMatch('child "email" fails because ["email" is required]');
+	});
+});
+
+describe('generateAuthToken funtion', () => {
+	it('should return a valid JWT', () => {
+		const user = {
+			id: 1,
+			userType: 'owner'
+		};
+		const token = generateAuthToken(user);
+		expect(token).toContain(['e'], '.');
+	});
+
+	it('should return a valid JWT ande decode it', () => {
+		const user = {
+			id: 1,
+			userType: 'owner'
+		};
+		const token = generateAuthToken(user);
+		const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+		expect(decoded).toMatchObject(user);
 	});
 });
